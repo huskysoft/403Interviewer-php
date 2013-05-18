eb50a962676e1978561eb662ac68622beabdb19f
Bennett Ng <bennett.k.ng@gmail.com>
2013-05-18 16:44:36 -0700

    debugged postQuestion

--- question/postQuestion.php
+++ question/postQuestion.php
@@ -3,32 +3,35 @@
 	* This script handles postQuestion requests. Requires user authentication.
 	* Returns the ID of the newly-created Question.
 	*/
+	
+	// require secure connection
+	if (!isset($_SERVER['HTTPS']) || !$_SERVER['HTTPS']) {
+		header("HTTP/1.1 301 Moved Permanently");
+		exit("Secure connection required");
+	}
 
 	// load dependencies
 	require('../util/requestParams.php');
 	require('../util/db_tables.php');	
 	require('../util/queryTools.php');
-       require('../util/security.php');
 	
-       // require secure connection
-       secureConnection();
-
+	$json = json_decode(file_get_contents('php://input'));
+	
 	// parse JSON payload
-	$authorId = filter_var($_POST[$COLUMN_QUESTION_AUTHORID], FILTER_SANITIZE_NUMBER_INT);
-	$questionText = filter_var($_POST[$COLUMN_QUESTION_TEXT], FILTER_SANITIZE_STRING);
-	$questionTitle = filter_var($_POST[$COLUMN_QUESTION_TITLE], FILTER_SANITIZE_STRING);
-	$questionDifficulty = filter_var($_POST[$COLUMN_QUESTION_DIFFICULTY], FILTER_SANITIZE_STRING);
-	$questionCategory = filter_var($_POST[$COLUMN_QUESTION_CATEGORY], FILTER_SANITIZE_STRING);
-	$dateCreated = filter_var($_POST[$COLUMN_QUESTION_DATE], FILTER_SANITIZE_NUMBER_INT);
+	$authorId = filter_var($json->$COLUMN_QUESTION_AUTHORID, FILTER_SANITIZE_NUMBER_INT);
+	$questionText = "'" . filter_var($json->$COLUMN_QUESTION_TEXT, FILTER_SANITIZE_STRING) . "'";
+	$questionTitle = "'" . filter_var($json->$COLUMN_QUESTION_TITLE, FILTER_SANITIZE_STRING) . "'";
+	$questionDifficulty = "'" . filter_var($json->$COLUMN_QUESTION_DIFFICULTY, FILTER_SANITIZE_STRING) . "'";
+	$questionCategory = "'" . filter_var($json->$COLUMN_QUESTION_CATEGORY, FILTER_SANITIZE_STRING) . "'";
+	$dateCreated = filter_var($json->$COLUMN_QUESTION_DATE, FILTER_SANITIZE_NUMBER_INT);
 		
 	// build query
-	$query = $INSERT . $TABLE_QUESTION . $VALUES;
+	$query = "INSERT INTO " . $TABLE_QUESTION . " VALUES ";
 	$query .= ("(DEFAULT, " . $authorId . ", " . $questionText . ", " . $questionTitle);
 	$query .= (", 0, 0, " . $questionDifficulty . ", " . $questionCategory);
 	$query .= (", " . $dateCreated . ")");
-	$query .= ($query . " RETURNING \"" . $COLUMN_QUESTION_QUESTIONID . "\"");
+	$query .= (" RETURNING '" . $COLUMN_QUESTION_QUESTIONID . "'");
 	
 	// execute query and return ID
-	$rs = executeQuery($query);
-	echo pg_fetch_result($rs, 0, 0);
+	echo executeQuery($query);
 ?>
